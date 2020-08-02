300646228bed0ca9d9c1994fcef301e741ba14cc
josephwkim <joekim987@gmail.com>
2020-08-01 17:55:41 -0700

    changed message

--- insights/src/App.js
+++ insights/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming soon: insights and analysis from the team at Viridity Capital
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://viriditycapital.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out our homepage
         </a>
       </header>
     </div>
