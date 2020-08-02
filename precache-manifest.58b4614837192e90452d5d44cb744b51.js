e75c2e2144c43beadc8f8440025c4c5f8aa071fb
josephwkim <joekim987@gmail.com>
2020-08-01 17:36:55 -0700

    Updates

---
+++ precache-manifest.58b4614837192e90452d5d44cb744b51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4384a995e489247f31833c88ed4e6e",
+    "url": "/insights/index.html"
+  },
+  {
+    "revision": "438c9640dc58fc0b2a10",
+    "url": "/insights/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d559340f7ecc10fe3511",
+    "url": "/insights/static/js/2.723ec508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/insights/static/js/2.723ec508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "438c9640dc58fc0b2a10",
+    "url": "/insights/static/js/main.10010c7e.chunk.js"
+  },
+  {
+    "revision": "73e52a6e910d6f81a435",
+    "url": "/insights/static/js/runtime-main.c9305b18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/insights/static/media/logo.5d5d9eef.svg"
+  }
+]);
